8b43cc5cbee3b4c10d26e892552c9dabd5c9e240
niki-soft <jan.mikulenka@gmail.com>
2022-03-07 12:25:38 +0100

    Založení projektu Dochazka

---
+++ Dochazka2/FlyoutMenuPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Dochazka2
+{
+    public partial class FlyoutMenuPage : ContentPage
+    {
+        public FlyoutMenuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Dochazka2/PhotoPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Dochazka2
+{
+    public partial class PhotoPage : ContentPage
+    {
+        public PhotoPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void CameraButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+    }
+}
