7564bbdc81041397ea9e597ef3b21b1f78626754
niki-soft <jan.mikulenka@gmail.com>
2022-03-07 16:46:42 +0100

    Drobné úpravy

--- Dochazka2/PhotoPage.xaml.cs
+++ Dochazka2/PhotoPage.xaml.cs
@@ -12,6 +12,14 @@ namespace Dochazka2
             InitializeComponent();
         }
 
+        public async void CameraButton1_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+
         public async void CameraButton_Clicked(System.Object sender, System.EventArgs e)
         {
             var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
